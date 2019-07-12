156adf605e5432d1852701fbcaf9b2f5cb219a40
Tin Duong <tinduong1809@gmail.com>
2019-07-11 23:41:12 -0600

    fixed AB#55

--- Source/SmartHotel.Clients/SmartHotel.Clients/Services/Booking/BookingService.cs
+++ Source/SmartHotel.Clients/SmartHotel.Clients/Services/Booking/BookingService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace SmartHotel.Clients.Core.Services.Booking
 {
+// another test comment
     public class BookingService : IBookingService
     {
         readonly IRequestService requestService;
